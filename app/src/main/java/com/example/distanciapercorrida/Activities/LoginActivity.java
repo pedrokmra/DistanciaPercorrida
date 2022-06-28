c1405d090348fe1f2dbe9f4db61e5654a77e6568
pedrokmra <pedrokimur@icloud.com>
2022-06-28 14:51:18 -0300

    login com autenticacao firebase

---
+++ app/src/main/java/com/example/distanciapercorrida/Activities/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.distanciapercorrida.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.distanciapercorrida.FirebaseLoginHelper;
+import com.example.distanciapercorrida.R;
+
+public class LoginActivity extends FirebaseLoginHelper {
+    private EditText email;
+    private EditText password;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findComponents();
+
+        buttonLogin.setOnClickListener(v -> {
+            boolean response;
+            response = login(email.getText().toString(), password.getText().toString());
+            startActivities(response);
+        });}
+
+    private void startActivities(boolean response) {
+        if (response) {
+            if (email.getText().toString().equals("pedro@admin.com")) {
+                startActivity(new Intent(getApplicationContext(), AdminActivity.class));
+            } else {
+                startActivity(new Intent(getApplicationContext(), OperatorActivity.class));
+            }
+        } else {
+            Toast.makeText(this, "Login Inválido", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void findComponents() {
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+    }
+}
---
+++ app/src/main/java/com/example/distanciapercorrida/FirebaseLoginHelper.java
@@ -0,0 +1,26 @@
+package com.example.distanciapercorrida;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class FirebaseLoginHelper extends AppCompatActivity {
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    boolean response;
+
+    protected boolean login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        response = true;
+                    } else {
+                        response = false;
+                    }
+                });
+        return response;
+    }
+
+    protected void logout() {
+        mAuth.signOut();
+        finish();
+    }
+}
